61801885d51a9ad458783825b7135f8cf3edbfce
Caedan <caedanlavender@gmail.com>
2022-04-04 08:49:48 +1200

    Added docker commands to shell script

--- docker.sh
+++ docker.sh
@@ -3,12 +3,12 @@ if [ $# -eq 0 ]; then
    echo "No arguments"
 else 
 if [ $1 = "build" ]; then
-   echo "build is successfull"
-   # docker build -t cypress-proto:1.0.3 .
+   echo "DOCKER BUILD"
+   docker build -t cypress-proto:1.0.3 .
 elif [ $1 = "run" ]; then
-   echo "run is successfull"
-   # docker run cypress-proto:1.0.3 .
+   echo "DOCKER RUN"
+   docker run cypress-proto:1.0.3 .
 else
-   echo "unknown argument"
+   echo "Unknown argument"
 fi
 fi
